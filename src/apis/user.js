180034c459ca659c652d0fd991ec4e1aa84ffcfa
MISAKA <3062803169@qq.com>
2024-08-06 10:28:00 +0800

    pinia用户数据持久化

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,7 +1,7 @@
 //封装所有和用户相关的接口函数
 import http from '@/utils/http'
 
-export const loginApi = ({ account, password }) => {
+export const loginAPI = ({ account, password }) => {
     return http({
         url: '/login',
         method: 'POST',
--- src/main.js
+++ src/main.js
@@ -9,7 +9,10 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(createPinia())
 app.use(router)
@@ -17,6 +20,10 @@ app.use(router)
 app.use(ElementPlus)
 
 app.mount('#app')
+
+//注册持久化插件
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 
 //测试接口函数
 import { getCategoryAPI } from './apis/testAPI'
